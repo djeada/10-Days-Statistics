7a08b66529eb2a53157baa2d2e94cd848bbcd592
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-10-31 19:19:40 +0100

    Day 0 Weighted Mean

---
+++ Day 0 Weighted Mean.py
@@ -0,0 +1,18 @@
+'''
+Given an array, X, N of  integers and an array, W , representing the respective weights of X's
+elements, calculate and print the weighted mean of X's elements.
+Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format).
+'''
+n = int(input())
+data = list(map(int, input().rstrip().split()))
+weighs = list(map(int, input().rstrip().split()))
+
+m = 0
+suma = 0
+
+for i in range(n):
+    m+= data[i]*weighs[i]
+    suma += weighs[i]
+m /= suma
+
+print('{:.1f}'.format(m))
