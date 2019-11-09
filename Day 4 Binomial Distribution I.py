1bda15146ef60ba355631456f9e7bcacfabd42d8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-09 23:45:25 +0100

    Day 4 Binomial Distribution I

---
+++ Day 4 Binomial Distribution I.py
@@ -0,0 +1,24 @@
+'''
+The ratio of boys to girls for babies born in Russia is 1.09:1.
+If there is 1 child born per birth, what proportion of Russian families
+with exactly 6 children will have at least  boys?
+
+Write a program to compute the answer using the above parameters.
+Then print your result, rounded to a scale of  decimal places
+(i.e., 1.234 format).
+'''
+
+def factorial(n):
+    if n < 2:
+        return 1
+    return n*factorial(n-1)
+
+def binomial(n, x, p):
+    return factorial(n) // factorial(n - x) // factorial(x) * (p ** x) * (1 - p) ** (n - x)
+
+
+boys, girls = map(float, input().split())
+
+probability = boys / (boys + girls)
+
+print('{:.3f}'.format( binomial(6, 3, probability) + binomial(6, 4, probability) + binomial(6, 5, probability) + binomial(6, 6, probability)))
