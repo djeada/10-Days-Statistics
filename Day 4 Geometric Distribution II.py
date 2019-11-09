59583b16d045ff70adb5c45a776d93218300f452
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-10 00:09:07 +0100

    Geometric Distribution

---
+++ Day 4 Geometric Distribution II.py
@@ -0,0 +1,18 @@
+'''
+The probability that a machine produces a defective product is 1/3.
+What is the probability that the 1st defect is found during the 5th inspection?
+'''
+
+def geometric(n, p):
+    return p * (1 - p) ** (n - 1)
+
+omega, space = map(int, input().split())
+p = omega/space
+
+n = int(input())
+
+sum = 0
+for i in range(1, 6):
+    sum += geometric(i, p)
+
+print('{:.3f}'.format(sum))
