3521eb664668c3f4100b135a0fecfefbb96a6fcf
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-10 00:20:42 +0100

    Poisson

---
+++ Day 5 Poisson Distribution I.py
@@ -0,0 +1,19 @@
+'''
+The probability that a machine produces a defective product is 1/3.
+What is the probability that the 1st defect is found during the 5th inspection?
+'''
+
+e = 2.718281828459
+
+def factorial(n):
+    if n < 2:
+        return 1
+    return n*factorial(n-1)
+
+def poisson(l, k):
+    return l** k * e ** (-l) / factorial(k)
+
+l = float(input())
+k = int(input())
+
+print('{:.3f}'.format(poisson(l, k)))
---
+++ Day 5 Poisson Distribution II.py
@@ -0,0 +1,13 @@
+'''
+The probability that a machine produces a defective product is 1/3.
+What is the probability that the 1st defect is found during the 5th inspection?
+'''
+
+def E(l):
+    return l + l ** 2
+
+l1, l2 = map(float, input().split())
+
+print('{:.3f}'.format(160 + 40 * E(l1)))
+print('{:.3f}'.format(128 + 40 * E(l2)))
+
