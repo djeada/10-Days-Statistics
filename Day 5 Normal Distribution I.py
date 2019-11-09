4d9ea51de6ebd20d920ff3aa480c133cdd4c1374
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-10 00:36:20 +0100

    Day 5 Normal Distribution

---
+++ Day 5 Normal Distribution I.py
@@ -0,0 +1,27 @@
+'''
+In a certain plant, the time taken to assemble a car is a random variable,X ,
+having a normal distribution with a mean of 20 hours and a standard deviation of
+2 hours. What is the probability that a car can be assembled at this plant in:
+Less than 19.5 hours?
+Between 20 and 22 hours?
+'''
+
+import math
+pi = 3.14
+
+def normalDistribution(x, miu, sigma):
+    return math.exp(- (x - miu) ** 2 / (2 * sigma ** 2)) / (sigma * math.sqrt(2 * pi))
+
+
+def cumulativeProbability(x, miu, sigma):
+    return 1 / 2 * (1 + math.erf((x - miu) / (sigma * math.sqrt(2))))
+
+
+miu, sigma = map(int, input().split())
+x = float(input())
+lower, upper = map(int, input().split())
+
+
+print('{:.3f}'.format(cumulativeProbability(x, miu, sigma)))
+
+print('{:.3f}'.format(cumulativeProbability(upper, miu, sigma) - cumulativeProbability(lower, miu, sigma)))
---
+++ Day 5 Normal Distribution II.py
@@ -0,0 +1,31 @@
+'''
+The final grades for a Physics exam taken by a large group
+of students have a mean of 70 and a standard deviation of 10.
+If we can approximate the distribution of these grades by
+a normal distribution, what percentage of the students:
+
+Scored higher than 80 (i.e., have a grade > 80)?
+Passed the test (i.e., have a grade >= 60)?
+Failed the test (i.e., have a grade < 60)?
+Find and print the answer to each question on a new line,
+rounded to a scale of 2 decimal places.
+'''
+import math
+pi = 3.14
+
+def normalDistribution(x, miu, sigma):
+    return math.exp(- (x - miu) ** 2 / (2 * sigma ** 2)) / (sigma * math.sqrt(2 * pi))
+
+
+def cumulativeProbability(x, miu, sigma):
+    return 1 / 2 * (1 + math.erf((x - miu) / (sigma * math.sqrt(2))))
+
+
+miu, sigma = map(int, input().split())
+x1 = float(input())
+x2 = float(input())
+
+print('{:.2f}'.format((100 -cumulativeProbability(x1, miu, sigma)*100)))
+print('{:.2f}'.format((100 - cumulativeProbability(x2, miu, sigma)*100)))
+print('{:.2f}'.format(cumulativeProbability(x2, miu, sigma)*100))
+
