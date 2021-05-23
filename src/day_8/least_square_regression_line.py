2992d67465834ca3edc03ff9edc073a229e77b06
djeada <addjellouli1@gmail.com>
2021-05-23 14:09:19 +0200

    improved solutions day 8

--- src/day_8/least_square_regression_line.py
+++ src/day_8/least_square_regression_line.py
@@ -1,7 +1,7 @@
 """
 A group of five students enrolls in Statistics immediately after
-taking a Math aptitude test. Each student's Math aptitude test score, x,
-and Statistics course grade, y , can be expressed as the following list
+taking a Math aptitude test. Each student's Math aptitude test score, X,
+and Statistics course grade, Y, can be expressed as the following list
 of  points.
 If a student scored an 80 on the Math aptitude test, what grade would
 we expect them to achieve in Statistics? Determine the equation of the
@@ -10,33 +10,42 @@ value of y when x = 80.
 """
 
 
-def linearRegression(X, Y, n):
-    sumX = 0
-    for i in range(n):
-        sumX += X[i]
-    sumXsq = 0
-    for i in range(n):
-        sumXsq += X[i] ** 2
-    sumY = 0
-    for i in range(n):
-        sumY += Y[i]
-    sumXY = 0
-    for i in range(n):
-        sumXY += X[i] * Y[i]
+def mean(arr):
+    return sum(arr) / len(arr)
 
-    b = (n * sumXY - sumX * sumY) / (n * sumXsq - sumX ** 2)
-    a = mean(Y) - b * mean(X)
+
+def linear_regression(x, y, n):
+
+    sum_x = sum(x)
+    sum_y = sum(y)
+
+    sum_xsq = 0
+    for elem in x:
+        sum_xsq += elem ** 2
+
+    sum_xy = 0
+    for elem_x, elem_y in zip(x, y):
+        sum_xy += elem_x * elem_y
+
+    b = (n * sum_xy - sum_x * sum_y) / (n * sum_xsq - sum_x ** 2)
+    a = mean(y) - b * mean(x)
+
     return a, b
 
 
-def mean(tab):
-    return sum(tab) / len(tab)
+def main():
+
+    n = 5
+    x, y = [0] * n, [0] * n
+
+    for i in range(n):
+        x[i], y[i] = map(int, input().split())
+
+    a, b = linear_regression(x, y, n)
+
+    result = a + b * 80
+    print("{:.3f}".format(result))
 
 
-X, Y = [0] * 5, [0] * 5
-for i in range(5):
-    X[i], Y[i] = map(int, input().split())
-
-a, b = linearRegression(X, Y, 5)
-
-print("{:.3f}".format(a + b * 80))
+if __name__ == "__main__":
+    main()
