e311ead9ef5ff1ad4615572fadc2be7d059a003b
djeada <addjellouli1@gmail.com>
2021-05-23 23:39:31 +0200

    improved solutions day 9

--- src/day_9/multiple_linear_regression.py
+++ src/day_9/multiple_linear_regression.py
@@ -1,42 +1,43 @@
 """
-A group of five students enrolls in Statistics immediately after
-taking a Math aptitude test. Each student's Math aptitude test score, x,
-and Statistics course grade, y , can be expressed as the following list
-of  points.
-If a student scored an 80 on the Math aptitude test, what grade would
-we expect them to achieve in Statistics? Determine the equation of the
-best-fit line using the least squares method, then compute and print the
-value of y when x = 80.
+Andrea has a simple equation:
+Y = a + b1f1 + b2f2 + ... + bmfm
+for (m + 1) real constants (a, f1, f2, ..., fm). We can say that the value of Y depends on m features. 
+Andrea studies this equation for n different feature sets (f1, f2, ..., fm) and records each respective value of Y. 
+If she has q new feature sets, can you help Andrea find the value of Y for each of the sets?
 """
 
-
-def linearRegression(X, Y, n):
-    sumX = 0
-    for i in range(n):
-        sumX += X[i]
-    sumXsq = 0
-    for i in range(n):
-        sumXsq += X[i] ** 2
-    sumY = 0
-    for i in range(n):
-        sumY += Y[i]
-    sumXY = 0
-    for i in range(n):
-        sumXY += X[i] * Y[i]
-
-    b = (n * sumXY - sumX * sumY) / (n * sumXsq - sumX ** 2)
-    a = mean(Y) - b * mean(X)
-    return a, b
+import numpy as np
 
 
-def mean(tab):
-    return sum(tab) / len(tab)
+def least_squares(x, y):
+    a = np.append(x, np.ones((len(x), 1)), axis=-1)
+    y = y[:, np.newaxis]
+
+    result = np.linalg.inv(np.dot(a.T, a))
+    result = np.dot(result, a.T)
+    result = np.dot(result, y)
+
+    return result[-1][0], result[:-1]
 
 
-X, Y = [0] * 5, [0] * 5
-for i in range(5):
-    X[i], Y[i] = map(int, input().split())
+def main():
 
-a, b = linearRegression(X, Y, 5)
+    m, n = list(map(int, str.split(input(), " ")))
+    data = [list(float(x) for x in input().split()) for i in range(n)]
 
-print("{:.3f}".format(a + b * 80))
+    x = np.array([[item[i] for i in range(m)] for item in data])
+    y = np.array([item[-1] for item in data])
+
+    a, b = least_squares(x, y)
+
+    q = int(input())
+
+    for i in range(q):
+        data = list(map(float, input().split()))
+        result = [b[j] * data[j] for j in range(m)]
+        result = a + np.sum(result)
+        print("{:.3f}".format(result))
+
+
+if __name__ == "__main__":
+    main()
