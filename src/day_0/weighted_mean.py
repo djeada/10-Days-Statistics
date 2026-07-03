dfe3f5ce548428e51c923d250a49c57987a3b719
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2026-07-03 18:42:56 +0200

    Update weighted_mean.py

--- src/day_0/weighted_mean.py
+++ src/day_0/weighted_mean.py
@@ -5,24 +5,18 @@ Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format).
 """
 
 
-def calculate_weighted_mean(data, weights, n):
-    m = 0
-    total = 0
-    for i in range(n):
-        m += data[i] * weights[i]
-        total += weights[i]
-    return m / total
+def calculate_weighted_mean(data, weights):
+    return sum(x * w for x, w in zip(data, weights)) / sum(weights)
 
 
 def main():
+    _ = int(input())
+    data = list(map(int, input().split()))
+    weights = list(map(int, input().split()))
 
-    n = int(input())
-    data = list(map(int, input().rstrip().split()))
-    weights = list(map(int, input().rstrip().split()))
+    weighted_mean = calculate_weighted_mean(data, weights)
 
-    weighted_mean = calculate_weighted_mean(data, weights, n)
-
-    print("{:.1f}".format(weighted_mean))
+    print(f"{weighted_mean:.1f}")
 
 
 if __name__ == "__main__":
