27646a353e130a28c99419748f9298ab87e91aff
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2026-07-04 11:30:33 +0200

    Update binomial_distribution_ii.py

--- src/day_4/binomial_distribution_ii.py
+++ src/day_4/binomial_distribution_ii.py
@@ -7,40 +7,22 @@ What is the probability that a batch of 10 pistons will contain:
 """
 
 
-def factorial(n):
-    if n < 2:
-        return 1
-
-    return n * factorial(n - 1)
+from math import comb
 
 
 def binomial(n, x, p):
-    return (
-        factorial(n) // factorial(n - x) // factorial(x) * (p ** x) * (1 - p) ** (n - x)
-    )
-
-
-def sum_binomials(n, p):
-    total = 0
-
-    for i in range(2, n + 1):
-        total += binomial(n, i, p)
-
-    return total
+    return comb(n, x) * p**x * (1 - p) ** (n - x)
 
 
 def main():
+    percent, n = map(int, input().split())
+    p = percent / 100
 
-    p, n = map(int, input().split())
-    p /= 100
+    no_more_than_2 = sum(binomial(n, x, p) for x in range(3))
+    at_least_2 = sum(binomial(n, x, p) for x in range(2, n + 1))
 
-    result = binomial(n, 0, p) + binomial(n, 1, p) + binomial(n, 2, p)
-
-    print("{:.3f}".format(result))
-
-    result = sum_binomials(n, p)
-
-    print("{:.3f}".format(result))
+    print(f"{no_more_than_2:.3f}")
+    print(f"{at_least_2:.3f}")
 
 
 if __name__ == "__main__":
