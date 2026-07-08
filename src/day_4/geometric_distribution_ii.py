8b73e231da78e04e8b5454cef45e9085a70723f1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2026-07-09 00:09:54 +0200

    Update geometric_distribution_ii.py

--- src/day_4/geometric_distribution_ii.py
+++ src/day_4/geometric_distribution_ii.py
@@ -3,19 +3,16 @@ The probability that a machine produces a defective product is 1/3.
 What is the probability that the 1st defect is found during the first 5 inspections?
 """
 
-def geometric_cdf(n, p):
-    return 1 - (1 - p) ** n
+def geometric_pmf(k, p):
+    return (1-p)**(k-1)*p
 
 
 def main():
     numerator, denominator = map(int, input().split())
     n = int(input())
-
-    p = numerator / denominator
-    result = geometric_cdf(n, p)
-
+    p = numerator/denominator
+    result = sum(geometric_pmf(k, p) for k in range(1, n+1))
     print(f"{result:.3f}")
-
 
 if __name__ == "__main__":
     main()
